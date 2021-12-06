29f508f23dd9f172a93cfdaa281a8cdfcce2910e
ultroNeous2021 <92087111+ultroNeous2021@users.noreply.github.com>
2021-12-06 15:38:24 +0530

    Initial Commit

---
+++ lib/push_notification.dart
@@ -0,0 +1,58 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+class PushNotification extends StatefulWidget {
+  const PushNotification({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  PushNotificationState createState() => PushNotificationState();
+}
+
+class PushNotificationState extends State<PushNotification> {
+  final FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+  @override
+  void initState() {
+    super.initState();
+    firebaseMessaging.getToken().then((token) {
+      print(token);
+    });
+  }
+
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
